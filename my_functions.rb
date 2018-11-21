c5c64d2df8089469beb52b4b191d9d9680003aaa
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-11-21 15:39:26 +0000

    Pass second test

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths(array1,array2)
   return array1.length()+array2.length()
 end
+
+def sum_array(array_of_numbers)
+ sum_of_array = 0
+  for number in array_of_numbers
+    sum_of_array += number
+  end
+  return sum_of_array
+end
